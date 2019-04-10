1624712f2261396f9be0bc44a811f441277f26a1
Matt Jablecki <mjablecki2@gmail.com>
2019-04-10 14:09:05 -0700

    Classes and Methods Drill

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a whole number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Classes/Classes/methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class methods
+    {
+        public Multiply(int input)
+        {
+            int answer1 = input * 2;
+        }
+        public Add(int input)
+        {
+            int answer2 = input + 5;
+        }
+        public Subtract(int input)
+        {
+            int answer3 = input - 1;
+        }
+    }
+}
