215e0f4cbf6b34906149c3d2fe3612debc40f870
Matt Jablecki <mjablecki2@gmail.com>
2019-04-12 16:15:44 -0700

    Upload Optional Parameter drill

---
+++ Optional Parameter/Optional Parameter/Optional.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameter
+{
+    class Optional
+    {
+        public static void Method(int required)
+        {
+            int answer = required + 5;
+            Console.WriteLine("The sum total of your input numbers plus 5 = " + answer);
+        }
+        public static void Method(int required, int optional)
+        {
+            int answer = required + optional + 5;
+            Console.WriteLine("The sum total of your input numbers plus 5 = " + answer);
+        }
+    }
+}
---
+++ Optional Parameter/Optional Parameter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please input a whole number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you so desire, go ahead and input a second whole number.");
+            string input = Console.ReadLine();
+            bool b1 = string.IsNullOrEmpty(input);
+            if (b1 == false)
+            {
+                int num2 = Convert.ToInt32(input);
+                Optional.Method(num1, num2);
+            }
+            else
+            {
+                Optional.Method(num1);
+            }            
+            Console.ReadLine();
+        }
+    }
+}
