886d8bd9a6d55159f6d55e30b9552a1ee0a6c019
Matt Jablecki <mjablecki2@gmail.com>
2019-04-12 14:52:24 -0700

    Upload 3 Methods Drill

---
+++ 3 Methods/3 Methods/Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Methods
+{
+    public class Methods
+    {
+        public static void Method(int Integer)
+        {
+            int answer = Integer + 1;
+            Console.WriteLine(Integer + " plus 1 = " + answer);
+        }
+        public static void Method(float Float)
+        {
+            float answer2 = Float * 5;
+            Console.WriteLine(Float + " times 5 = " + answer2);
+        }
+        public static void Method(string String)
+        {
+            try
+            {
+                int number = Convert.ToInt32(String);
+                int answer3 = number - 3;
+                Console.WriteLine(number + " minus 3 = " + answer3);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Invalid.");
+                return;
+            }
+        }
+    }
+}
---
+++ 3 Methods/3 Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input a whole number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Methods.Method(input);
+            Console.WriteLine("Please input a decimal.");
+            float input2 = float.Parse(Console.ReadLine());
+            Methods.Method(input2);
+            Console.WriteLine("Please input an integer.");
+            string input3 = Console.ReadLine();
+            Methods.Method(input3);
+            Console.ReadLine();
+        }
+
+    }
+}
