bcb57f4a1988ebb9e8799e9ca213b7e5629ab879
Matt Jablecki <mjablecki2@gmail.com>
2019-04-15 12:50:12 -0700

    Class Methods Drill

---
+++ Class Methods/Class Methods/Method.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods
+{
+    static class Method
+    {
+        public static void Divide (int parameter)
+        {
+            
+            int answer = parameter / 2;
+            Method.Divide(parameter, answer);
+
+        }
+        public static void Divide(int parameter, int answer)
+        {
+            Console.WriteLine(parameter + " divided by 2 = " + answer);
+            Console.ReadLine();
+
+        }
+
+    }
+}
