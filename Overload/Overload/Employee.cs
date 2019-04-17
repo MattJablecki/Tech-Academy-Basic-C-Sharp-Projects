7b51a1423e031f9e7ef26f60885bc137e55595ec
Matt Jablecki <mjablecki2@gmail.com>
2019-04-17 12:08:05 -0700

    Overload Drill

---
+++ Overload/Overload/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    public class Employee : Person
+    {
+        //public static Employee operator== (Employee employee, Employee employee1)
+        //{
+        //    if (employee.ID == employee1.ID)
+        //    {
+        //        Console.WriteLine("ID match.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("ID does not match.");
+        //    }
+
+        //}
+        //public static Employee operator!= (Employee employee, Employee employee1)
+        //{
+            
+        //}
+
+
+    }
+}
---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.ID = 1;
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Student";
+            employee1.LastName = "Sample";
+            employee1.ID = 2;
+            employee.SayName();
+            employee1.SayName();
+        }
+    }
+}
