81c5735ec445f2ac246cec15b8dfd71e5ce97e84
Matt Jablecki <mjablecki2@gmail.com>
2019-04-18 12:09:47 -0700

    Changes made

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -11,9 +11,21 @@ namespace Generics
         public static void Main()
         {
             Employee<string> employee = new Employee<string> ();
-            employee.Things;
+            employee.Things.Add("Joe");
+            employee.Things.Add("Bill");
+            employee.Things.Add("Kyle");
             Employee<int> ID = new Employee<int>();
-            ID.Things;            
+            ID.Things.Add(1);
+            ID.Things.Add(2);
+            ID.Things.Add(3);
+            foreach (string employee in employee.Things)
+            {
+                Console.WriteLine(employee);
+            }
+            foreach (int ID in ID.Things)
+            {
+                Console.WriteLine(ID);
+            }
         }
     }
 }
