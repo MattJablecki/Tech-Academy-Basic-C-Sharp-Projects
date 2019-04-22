75d987600afe287393db3b64d4475764e6b4e077
Matt Jablecki <mjablecki2@gmail.com>
2019-04-22 16:28:59 -0700

    Const Var Drill

---
+++ ConstVar/ConstVar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Company = "The ACME Factory";
+            var Year = 2019;
+            Employee employee1 = new Employee(5023, "Joe Jackson");
+            Employee employee2 = new Employee("Lisa Turtle");
+            Console.WriteLine("Welcome to " + Company + ". \nEstablished in " + Year + ", we have two great employees we'd love to recognize. \n" + employee1.name + " & " + employee2.name + "\nThanks for being great employees!");
+            Console.ReadLine();
+        }
+
+    }
+}
