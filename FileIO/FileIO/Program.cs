dce489993681643cd373426c511bc1f2d2dad4c3
Matt Jablecki <mjablecki2@gmail.com>
2019-04-22 14:18:16 -0700

    File IO Drill

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number.");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Matt\number.txt", number);
+            Console.WriteLine("You selected " + number);
+            Console.ReadLine();
+            File.ReadAllText(@"C:\Users\Matt\number.txt");
+        }
+    }
+}
