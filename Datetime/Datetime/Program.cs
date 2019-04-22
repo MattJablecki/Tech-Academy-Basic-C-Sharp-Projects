473ebb48af964b30d33e613622f71f84f9686a99
Matt Jablecki <mjablecki2@gmail.com>
2019-04-22 14:39:26 -0700

    DateTime Drill

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The Time is currently " + now);
+            Console.WriteLine("Choose a number. This will be how many hours in the future you would like to travel.");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime future = now.AddHours(hours);
+            Console.WriteLine("Whoa dude! It is now " + future + ". Somebody find Doc Brown!");
+            Console.ReadLine();
+
+        }
+    }
+}
